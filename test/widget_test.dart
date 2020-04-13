d00020b32398e5e2d7206ebc1e7756fb032737a6
WingCH <jason021075@me.com>
2020-04-13 21:12:12 +0800

    add test case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,26 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutterreduxbase/main.dart';
+import 'package:flutterreduxbase/redux/app/app_reducer.dart';
+import 'package:flutterreduxbase/redux/app/app_state.dart';
+import 'package:flutterreduxbase/redux/counter/counter_middleware.dart';
+import 'package:redux/redux.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(
+      ReduxApp(
+        store: Store<AppState>(
+          appReducer,
+          initialState: AppState.initial(),
+          middleware: [
+            CounterMiddleware(),
+          ],
+        ),
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -21,7 +34,9 @@ void main() {
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+
+    // Delay 1 second because count will increase after one second [counter_middleware]
+    await tester.pumpAndSettle(const Duration(seconds: 1));
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
